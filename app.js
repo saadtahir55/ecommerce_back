070882dba07008ac3bb133aaf5c5e7eeb216c806
saadtahir55 <140590643+saadtahir55@users.noreply.github.com>
2023-07-26 18:23:31 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+let app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.listen(5000, ()=>{
+    console.log('Server is Running');
+})
+
+
+
+
+
+
+
+
+
+
